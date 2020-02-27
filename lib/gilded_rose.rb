c934de77810956f0e26d223e82dbe7e6529b371a
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-27 11:33:22 +0000

    move increase/decrease quality, decrease sell_in, condition method to ItemProperty class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'item'
 require_relative 'item_property'
-class GildedRose
-  MAX_QUALITY = 50
-  MIN_QUALITY = 0
-  DEFAULT_QUALITY_INCREASE = 1
-  DEFAULT_QUALITY_DECREASE = 1
-  DEFAULT_SELL_IN_DECREASE = 1
+class GildedRose < ItemProperty
 
   def initialize(items)
     @items = items
@@ -16,44 +10,6 @@ class GildedRose
   def update_quality
     @items.each do |item|
      condition(item)
-    end
-  end
-
-  def increase_quality(item, amount=DEFAULT_QUALITY_INCREASE)
-    item.quality += amount if item.quality < MAX_QUALITY
-  end
-
-  def decrease_quality(item, amount=DEFAULT_QUALITY_DECREASE)
-    item.quality -= amount if item.quality > MIN_QUALITY
-  end
-
-  def decrease_sell_in(item, amount=DEFAULT_SELL_IN_DECREASE)
-    item.sell_in -= amount
-  end
-
-  def condition(item)
-    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-      decrease_quality(item) if item.name != 'Sulfuras, Hand of Ragnaros'
-    else
-      if item.quality < 50
-        increase_quality(item)
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          increase_quality(item) if item.sell_in < 11
-          increase_quality(item) if item.sell_in < 6 
-        end
-      end
-    end
-    decrease_sell_in(item) if item.name != 'Sulfuras, Hand of Ragnaros'
-    if item.sell_in < 0
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          decrease_quality(item) if item.name != 'Sulfuras, Hand of Ragnaros'
-        else
-          decrease_quality(item, item.quality)
-        end
-      else
-        increase_quality(item)
-      end
     end
   end
 
--- lib/item_property.rb
+++ lib/item_property.rb
@@ -1,8 +1,48 @@
+require_relative 'item'
+
 class ItemProperty
   MAX_QUALITY = 50
+  MIN_QUALITY = 0
   DEFAULT_QUALITY_INCREASE = 1
+  DEFAULT_QUALITY_DECREASE = 1
+  DEFAULT_SELL_IN_DECREASE = 1
 
-  def increase_quality(amount=DEFAULT_QUALITY_INCREASE)
-    self.quality += amount if self.quality < MAX_QUALITY
+  def increase_quality(item, amount=DEFAULT_QUALITY_INCREASE)
+    item.quality += amount if item.quality < MAX_QUALITY
   end
+
+  def decrease_quality(item, amount=DEFAULT_QUALITY_DECREASE)
+    item.quality -= amount if item.quality > MIN_QUALITY
+  end
+
+  def decrease_sell_in(item, amount=DEFAULT_SELL_IN_DECREASE)
+    item.sell_in -= amount
+  end
+
+  def condition(item)
+    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+      decrease_quality(item) if item.name != 'Sulfuras, Hand of Ragnaros'
+    else
+      if item.quality < 50
+        increase_quality(item)
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          increase_quality(item) if item.sell_in < 11
+          increase_quality(item) if item.sell_in < 6 
+        end
+      end
+    end
+    decrease_sell_in(item) if item.name != 'Sulfuras, Hand of Ragnaros'
+    if item.sell_in < 0
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          decrease_quality(item) if item.name != 'Sulfuras, Hand of Ragnaros'
+        else
+          decrease_quality(item, item.quality)
+        end
+      else
+        increase_quality(item)
+      end
+    end
+  end
+
 end
